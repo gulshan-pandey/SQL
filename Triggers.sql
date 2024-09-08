d9624f073c0a3dc508263bc19403aef289b456e9
gulshan-pandey <gulshanpandey7210@gmail.com>
2024-09-08 21:28:49 +0530

    Triggers completed

--- Triggers.sql
+++ Triggers.sql
@@ -1,5 +1,6 @@
 
 
+
 CREATE TABLE Salary (
     Employee_ID INT PRIMARY KEY,
     First_Name VARCHAR(50),
@@ -45,22 +46,30 @@ SET Hourly_pay = Hourly_pay + 5; -- This updates the employee with ID 1
 -- creating a new trigger whenever we add new employee the the yearly pay is instantiated 
 
 
-
 DELETE FROM Salary
 WHERE Employee_ID = 6; 
 
-CREATE TRIGGER before_Hourly_pay_insert				-- before_Hourly_pay_insert :- this is just a name of trigger and dont have to do anything with the behaviour of the trigger
+-- before_Hourly_pay_insert :- this is just a name of trigger and dont have to do anything with the behaviour of the trigger
+
+CREATE TRIGGER before_Hourly_pay_insert				
+BEFORE UPDATE ON Salary
+FOR EACH ROW
+SET NEW.YEARLY =(NEW.Hourly_Pay * 1920);
+
+CREATE TRIGGER insert_Hourly_pay_insert				
 BEFORE INSERT ON Salary
 FOR EACH ROW
 SET NEW.YEARLY =(NEW.Hourly_Pay * 1920);
 
 INSERT INTO Salary VALUES
-(6, 'Jane', 'Smith', 18.75,NULL, 'Accountant', '2019-06-20');
+(0, 'Jane', 'Smith', 18.75,NULL, 'Accountant', '2019-06-20');
 
 SELECT * FROM Salary;
  SHOW TRIGGERS;
  
  drop table expenses;
+ 
+ -- creating new table expense and adding triggers on it--------------------------
  
 CREATE TABLE expenses(
 	expense_id INT PRIMARY KEY,
@@ -68,7 +77,7 @@ CREATE TABLE expenses(
     expense_total DECIMAL(20,2)
 );
 
-
+DROP TABLE expenses;
 SELECT * FROM expenses;
 INSERT  INTO expenses 
 VALUES (1,'budget',0),
@@ -95,6 +104,9 @@ SELECT * FROM Salary;
 
 DELETE FROM Salary WHERE Employee_ID = 2;
 
+
+-- Creating trigger for adding adding the yearly salary to the budget of table expense whenever we insert new employee to the Salary table 
+
 CREATE TRIGGER after_salary_insert
 AFTER INSERT ON Salary
 FOR EACH ROW
@@ -103,14 +115,32 @@ SET expense_total = expense_total + NEW.Yearly
 WHERE expense_name = 'budget';
 
 
-DROP TRIGGER IF EXISTS after_salary_insert;
+DROP TRIGGER IF EXISTS after_salary_delete	;
 
 
 SELECT * FROM expenses;
 
 INSERT INTO Salary (Employee_ID, First_Name, Last_Name, Hourly_Pay, Job, Hire_Date) VALUES
-(2, 'rani', 'jhon', 29.75, 'HR', '2011-06-20');
+(1, 'John', 'Doe', 100, 'Software Engineer', '2020-01-15');
 
 SHOW TRIGGERS;
 
 -- creating a trigger whenver any change in yearly row happens it will update the whole budget of the expenses table
+
+CREATE TRIGGER on_Yearly_update
+AFTER UPDATE ON Salary
+FOR EACH ROW
+UPDATE expenses
+SET expense_total = expense_total + (New.Yearly -OLD.Yearly)
+WHERE expense_name = 'budget';
+
+
+UPDATE Salary
+SET Hourly_Pay =99
+WHERE Employee_ID =1;
+
+SELECT * FROM expenses;
+SELECT * FROM Salary;
+
+DELETE FROM Salary WHERE Employee_ID=1;
+
